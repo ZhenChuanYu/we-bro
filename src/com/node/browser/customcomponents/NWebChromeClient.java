7615cd22a47eeb0ccc4de7f7f6f6f1ae491be359
zhenchuan <yuzhenchuan1@gmail.com>
2013-09-21 00:49:35 +0800

    delete unuse components

--- src/com/node/browser/customcomponents/NWebChromeClient.java
+++
@@ -1,7 +0,0 @@
-package com.node.browser.customcomponents;
-
-import android.webkit.WebChromeClient;
-
-public class NWebChromeClient extends WebChromeClient{
-	
-}
--- src/com/node/browser/customcomponents/NWebViewClient.java
+++
@@ -1,7 +0,0 @@
-package com.node.browser.customcomponents;
-
-import android.webkit.WebViewClient;
-
-public class NWebViewClient extends WebViewClient {
-
-}
